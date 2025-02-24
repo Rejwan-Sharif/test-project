581e90ea14442d55391f976902bc9d0f49b9e878
Rejwan Sharif <mohammedrejwan8@gmail.com>
2025-02-25 00:03:44 +0600

    Exception Handler Added

--- src/main/java/com/test/errorHandling/GlobalExceptionHandler.java
+++ src/main/java/com/test/errorHandling/GlobalExceptionHandler.java
@@ -1,18 +1,19 @@
 package com.test.errorHandling;
 
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,7 +22,19 @@ public class GlobalExceptionHandler {
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ErrorResponseDto responseNotFound(ResourceNotFoundException notFoundException){
+    public Object responseNotFound(HttpServletRequest request, ResourceNotFoundException notFoundException){
+        List<Object> accept = Collections.singletonList(request.getHeader(HttpHeaders.ACCEPT));
+
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("timestamp", Instant.now());
+        errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorResponse.put("error", MathException.class.getSimpleName());
+//        errorResponse.put("message", ResourceNotFoundException.getMessage());
+
+        if (!accept.contains(MediaType.TEXT_HTML_VALUE)){
+            return new ModelAndView("error",
+                   errorResponse );
+        }
         return new ErrorResponseDto(
                 HttpStatus.NOT_FOUND.value(),
                 HttpStatus.NOT_FOUND.toString(),
